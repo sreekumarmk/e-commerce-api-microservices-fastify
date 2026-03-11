b8709cbe40f1de0d0ceb026be6f1b46e334c2942
Sreekumar M K <sruthy.m@thinkpalm.com>
2026-03-11 17:49:41 +0530

    feat: Configure Terraform S3 backend for state management and initialize backend setup files.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket         = "ecommerce-terraform-state-505833152145"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
